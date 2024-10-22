5a09906dd1a32ef089f1969041f24c80d2c181ca
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-10-22 20:07:16 +0100

    Initial project push

---
+++ UnrealPractice/Source/UnrealPractice/UnrealPractice.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealPractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealPractice, "UnrealPractice" );
+ 
---
+++ UnrealPractice/Source/UnrealPractice/UnrealPracticeGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealPracticeGameMode.h"
+#include "UnrealPracticeCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUnrealPracticeGameMode::AUnrealPracticeGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
